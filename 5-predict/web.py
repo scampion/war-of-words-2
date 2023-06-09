1bfbb0387d618321d82b3ce0d8257536b73cc327
Sebastien Campion <sebastien.campion@gmail.com>
2023-06-09 09:27:22 +0200

    udpate

--- 5-predict/web.py
+++ 5-predict/web.py
@@ -23,6 +23,8 @@ if os.path.exists('ep8-all_features-text-chronological.predict'):
 
 else:
     model_path = '../2-training/trained-models/ep8-all_features-text-chronological.predict'
+    predict.model_edit = fasttext.load_model('../../data/ep' + legislature + '-' + task + '-edit.bin')
+    predict.model_title = fasttext.load_model('../../data/ep' + legislature + '-' + task + '-title.bin')
 
 
 st.title('🪖 War of Words 💬')
@@ -30,7 +32,8 @@ st.subheader('Predicting the success of amendment in the European Parliament')
 st.write('Beta version')
 st.write('This is a **beta application** based on the War of Words project. '
          'The goal of this project was to study the dynamics of the legislative process in the European Parliament.'
-         'We propose to use the machine learning model implemented in that scientific article for predicting the success of amendment')
+         'We propose to use the machine learning model implemented in that scientific article for predicting the success of amendment'
+         '⚠️ results of that proof of concept can\'t be used for a daily works, we already detect bugs and limitations.' )
 
 st.markdown(
     '> Kristof, V., Grossglauser, M., Thiran, P., '
@@ -89,6 +92,7 @@ def amlist_to_dataframe(amlist):
     for i, (datum, score) in enumerate(amlist):
         am = 'statuquo' if type(datum) == str and datum == 'statuquo' else 'amendment ' + datum.get('amendment_num', '')
         df.append({'amendment': am,
+                   'edit': datum.get('edit_id', '') if type(datum) == dict else '',
                    'stars': get_stars(score),
                    'probability in %': int(score * 100)})
     return pd.DataFrame(df)
@@ -127,50 +131,87 @@ for article, am_list in structure(docx_content).items():
     st.write(styler.to_html(), unsafe_allow_html=True)
     st.write('---')
 
-if st.button('Show details'):
 
-    st.markdown('## Details')
-    current_article = None
+st.markdown('## Details')
+current_article = None
 
-    for i, (article, datum, score) in enumerate(predict.main(docx_content, model_path)):
-        if article != current_article:
-            st.write('---')
-            st.markdown(f'## Article {article}')
-            current_article = article
-            df = pd.DataFrame()
+for i, (article, datum, score) in enumerate(predict.main(docx_content, model_path)):
+    if article != current_article:
+        st.write('---')
+        st.markdown(f'## Article {article}')
+        current_article = article
+        df = pd.DataFrame()
 
-        if type(datum) == str and datum == 'statuquo':
-            st.markdown('### Statu quo')
-        else:
-            st.markdown('### Amendment ' + datum.get('amendment_num', ' '))
-            try:
-                st.markdown("**Author(s):** " + ', '.join([a['name'] + ' (' + a['group']+')' for a in datum.get('authors', ' ')]))
-                st.markdown("**Edit type:** " + datum.get('edit_type', ' '))
-                col1, col2 = st.columns(2)
+    if type(datum) == str and datum == 'statuquo':
+        st.markdown('### Statu quo')
+    else:
+        st.markdown('### Amendment ' + datum.get('amendment_num', ' ') + ' Edit ' + str(datum.get('edit_id', '')))
+        try:
+            st.markdown("**Author(s):** " + ', '.join([a['name'] + ' (' + a['group']+')' for a in datum.get('authors', ' ')]))
+            st.markdown("**Edit type:** " + datum.get('edit_type', ' '))
+            col1, col2 = st.columns(2)
+            if datum.get('edit_type', ' ') == 'delete':
                 with col1:
                     st.markdown("#### Text original")
-                    st.markdown(' '.join(datum['text_original']))
+                    b = datum['text_original'].split()[:datum['edit_indices']['i1']]
+                    m = datum['text_original'].split()[datum['edit_indices']['i1']:datum['edit_indices']['i2']]
+                    e = datum['text_original'].split()[datum['edit_indices']['i2']:]
+                    annotated_text(' '.join(b), (' '.join(m), 'deleted', "#faa"), ' '.join(e))
+                    st.json([datum['text_original']])
+                    st.json(datum['edit_indices'])
                 with col2:
                     st.markdown("#### Text amended")
-                    st.markdown(' '.join(datum['text_amended']))
-                    # annotated_text(
-                    #     "This ",
-                    #     ("is", "verb"),
-                    #     " some ",
-                    #     ("annotated", "adj"),
-                    #     ("text", "noun"),
-                    #     " for those of ",
-                    #     ("you", "pronoun"),
-                    #     " who ",
-                    #     ("like", "verb"),
-                    #     " this sort of ",
-                    #     ("thing", "noun"),
-                    #     "."
-                    # )
-                # st.json(datum)
-            except Exception as e:
-                st.markdown('Error ' + str(e))
-        st.write(f'Probability of success: {score:.4f}')
-        st.progress(int(score * 100), text=str(int(score * 100)))
+                    st.markdown(datum['text_amended'])
+            elif datum.get('edit_type', ' ') == 'insert':
+                with col1:
+                    st.markdown("#### Text original")
+                    st.markdown(datum['text_original'])
+                with col2:
+                    st.markdown("#### Text amended")
+                    b = datum['text_amended'].split()[:datum['edit_indices']['j1']]
+                    m = datum['text_amended'].split()[datum['edit_indices']['j1']:datum['edit_indices']['j2']]
+                    e = datum['text_amended'].split()[datum['edit_indices']['j2']:]
+                    annotated_text(' '.join(b), (' '.join(m), 'inserted', "#afa"), ' '.join(e))
+                    st.json([datum['text_amended']])
+                    st.json(datum['edit_indices'])
+            elif datum.get('edit_type', ' ') == 'replace':
+                with col1:
+                    st.markdown("#### Text original")
+                    b = datum['text_original'].split()[:datum['edit_indices']['i1']]
+                    m = datum['text_original'].split()[datum['edit_indices']['i1']:datum['edit_indices']['i2']]
+                    e = datum['text_original'].split()[datum['edit_indices']['i2']:]
+                    annotated_text(' '.join(b), (' '.join(m), 'deleted', "#faa"), ' '.join(e))
+                    st.json([datum['text_original']])
+                    st.json(datum['edit_indices'])
+                with col2:
+                    st.markdown("#### Text amended")
+                    b = datum['text_amended'].split()[:datum['edit_indices']['j1']]
+                    m = datum['text_amended'].split()[datum['edit_indices']['j1']:datum['edit_indices']['j2']]
+                    e = datum['text_amended'].split()[datum['edit_indices']['j2']:]
+                    annotated_text(' '.join(b), (' '.join(m), 'inserted', "#afa"), ' '.join(e))
+                    st.json([datum['text_amended']])
+                    st.json(datum['edit_indices'])
+
+
+
+                # annotated_text(
+                #     "This ",
+                #     ("is", "verb"),
+                #     " some ",
+                #     ("annotated", "adj"),
+                #     ("text", "noun"),
+                #     " for those of ",
+                #     ("you", "pronoun"),
+                #     " who ",
+                #     ("like", "verb"),
+                #     " this sort of ",
+                #     ("thing", "noun"),
+                #     "."
+                # )
+            # st.json(datum)
+        except Exception as e:
+            st.markdown('Error ' + str(e))
+    st.write(f'Probability of success: {score:.4f}')
+    st.progress(int(score * 100), text=str(int(score * 100)))
 
 st.markdown(open('docs.md').read())
